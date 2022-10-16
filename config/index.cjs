f9d7202c4a749a91f77e221927dff0e2cc206b9c
mfyzs <115920491+mfyzs@users.noreply.github.com>
2022-10-16 20:57:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2001', date: '12-13',
+          type: '*生日', name: '乖乖', year: '2001', date: '12-13',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2002', date: '01-25',
+          type: '生日', name: '乖乖', year: '2002', date: '01-25',
         },
        
       ],
