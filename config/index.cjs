8de96094e9733f39cd327277f5ac856bb8d6ba7a
mfyzs <115920491+mfyzs@users.noreply.github.com>
2022-10-16 21:01:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '乖乖',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o3ZSs6cpWN6oiLGyD7rDahL7nyfk',
+      id: 'o3ZSs6c9sWbRSAakeTRdiF7AJotk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '8LRzllYut5n9y7yGXPoawByLJBOvAnzo2lhWkNbVZAg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
